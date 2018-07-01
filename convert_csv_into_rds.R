1c4ed1cc1373554434c5f1a64c714d984cbc2400
Alberto Moreno <albmor1989@gmail.com>
2018-07-01 15:01:04 +0000

    .

--- convert_csv_into_rds.R
+++ convert_csv_into_rds.R
@@ -6,3 +6,16 @@ rds_path <- "/home/rstudio/data/meg_mci.rds"
 
 original_dataset <- read_csv(file_path)
 saveRDS(original_dataset, rds_path)
+
+data_mean <- original_dataset %>% select(id, class, ends_with("mean"))
+data_median <- original_dataset %>% select(id, class, ends_with("median"))
+data_std <- original_dataset %>% select(id, class, ends_with("std"))
+data_mad <- original_dataset %>% select(id, class, ends_with("mad"))
+data_cov <- original_dataset %>% select(id, class, ends_with("cov"))
+
+saveRDS(data_mean, "/home/rstudio/data/mean.rds")
+saveRDS(data_median, "/home/rstudio/data/median.rds")
+saveRDS(data_std, "/home/rstudio/data/std.rds")
+saveRDS(data_mad, "/home/rstudio/data/mad.rds")
+saveRDS(data_cov, "/home/rstudio/data/cov.rds")
+
